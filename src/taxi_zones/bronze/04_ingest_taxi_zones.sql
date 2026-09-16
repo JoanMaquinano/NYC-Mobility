ef0b789a2ffb3914da3a7c0e0c61f64795e43f57
Joan Maquinano <158557104+JoanMaquinano@users.noreply.github.com>
2026-09-16 15:08:25 +0800

    Update 04_ingest_taxi_zones.sql

--- src/taxi_zones/bronze/04_ingest_taxi_zones.sql
+++ src/taxi_zones/bronze/04_ingest_taxi_zones.sql
@@ -1,12 +1,13 @@
-CREATE TABLE IF NOT EXISTS workspace.taxi_bronze.taxi_zone_may2026
-USING DELTA
-TBLPROPERTIES ('delta.columnMapping.mode' = 'name')
-AS
-SELECT *,
-  current_timestamp() AS ingestion_time,
-  _metadata.file_path AS source_file
+INSERT INTO `nyc-mobility`.nyc_bronze.taxi_zones
+SELECT
+    LocationID,
+    Borough,
+    Zone,
+    service_zone,
+    current_timestamp() AS ingestion_time,
+    _metadata.file_path AS source_file
 FROM read_files(
-  '/Volumes/workspace/default/ftw-b12-de/groups/week-08/group-d/taxi-zone-lookup/',
-  format => 'csv',
-  header => 'true'
-)
+    '/Volumes/workspace/default/ftw-b12-de/groups/week-08/group-d/taxi-zone-lookup/',
+    format => 'csv',
+    header => 'true'
+);
