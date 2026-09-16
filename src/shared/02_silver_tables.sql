d6158c73d2b25f6f7d4592f3516c5eb8a97ff68c
joan.maquinano@ftwfoundation.org <joan.maquinano@ftwfoundation.org>
2026-09-16 10:21:28 +0000

    Populate silver tables

--- src/shared/02_silver_tables.sql
+++ src/shared/02_silver_tables.sql
@@ -1,4 +1,6 @@
--- Create silver taxi_zones table
+-- Create silver green_taxi_clean table
+
+-- Create silver taxi_zones_clean table
 CREATE TABLE IF NOT EXISTS `nyc-mobility`.nyc_silver.taxi_zones_clean
 (
     location_id INT,
@@ -11,3 +13,6 @@ USING DELTA
 TBLPROPERTIES (
     'delta.columnMapping.mode' = 'name'
 );
+-- Create silver traffic_advisories_clean table
+
+-- Create silver weather_clean table​‌
