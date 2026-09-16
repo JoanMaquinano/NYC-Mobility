98317cc2b8c607077a8bcfe55a87454809a8cf09
jessy <jess.manuel@ftwfoundation.org>
2026-09-16 22:21:22 +0800

    Refactor green_taxi and weather tables to clean versions
    Removed the old green_taxi table definition and added a new green_taxi_clean table with updated schema. Also modified the weather table to create a weather_clean version.

--- src/shared/02_silver_tables.sql
+++ src/shared/02_silver_tables.sql
@@ -1,48 +1,35 @@
--- Create silver taxi_zones table
-CREATE TABLE IF NOT EXISTS `nyc-mobility`.nyc_silver.taxi_zones_clean
-(
-    location_id INT,
-    borough STRING,
-    zone STRING,
-    service_zone STRING,
-    ingestion_time TIMESTAMP
+
+-- Create silver green_taxi
+CREATE TABLE IF NOT EXISTS 'nyc-mobility'.nyc_silver.green_taxi_clean (
+    vendor_id INT,
+    lpep_pickup_datetime TIMESTAMP,
+    lpep_dropoff_datetime TIMESTAMP,
+    store_and_fwd_flag STRING,
+    ratecode_id INT,
+    pu_location_id INT,
+    do_location_id INT,
+    passenger_count INT,
+    trip_distance DOUBLE,
+    fare_amount DOUBLE,
+    extra DOUBLE,
+    mta_tax DOUBLE,
+    tip_amount DOUBLE,
+    tolls_amount DOUBLE,
+    improvement_surcharge DOUBLE,
+    total_amount DOUBLE,
+    payment_type INT,
+    trip_type INT,
+    congestion_surcharge DOUBLE,
+    cbd_congestion_fee DOUBLE,
+    qc_error_descriptions ARRAY<STRING>
 )
 USING DELTA
 TBLPROPERTIES (
     'delta.columnMapping.mode' = 'name'
 );
 
--- Create silver taxi_zones table
-CREATE TABLE IF NOT EXISTS nyc-mobility.nyc_silver.green_taxi (
-vendor_id INT,
-lpep_pickup_datetime TIMESTAMP,
-lpep_dropoff_datetime TIMESTAMP,
-store_and_fwd_flag STRING,
-ratecode_id INT,
-pu_location_id INT,
-do_location_id INT,
-passenger_count INT,
-trip_distance DOUBLE,
-fare_amount DOUBLE,
-extra DOUBLE,
-mta_tax DOUBLE,
-tip_amount DOUBLE,
-tolls_amount DOUBLE,
-improvement_surcharge DOUBLE,
-total_amount DOUBLE,
-payment_type INT,
-trip_type INT,
-congestion_surcharge DOUBLE,
-cbd_congestion_fee DOUBLE,
-qc_error_descriptions ARRAY
-)
-USING DELTA
-TBLPROPERTIES (
-'delta.columnMapping.mode' = 'name'
-);
-
 -- Create silver weather table
-CREATE TABLE IF NOT EXISTS `nyc-mobility`.nyc_silver.weather (
+CREATE TABLE IF NOT EXISTS `nyc-mobility`.nyc_silver.weather_clean (
     date STRING,
     temperature_2m STRING,
     apparent_temperature STRING,
