2505b4a81dc0442ce41edb5c929ccb953a35240e
jessy <jess.manuel@ftwfoundation.org>
2026-09-16 17:34:20 +0800

    Rename columns in taxi_zones table
    `LocationID` and `Borough` use PascalCase, while other columns use `snake_case`. Consider updating them to `location_id` and `borough` to match `02_silver_tables.sql`

--- src/shared/01_bronze_tables.sql
+++ src/shared/01_bronze_tables.sql
@@ -1,9 +1,9 @@
 -- Create bronze taxi_zones table
 CREATE TABLE IF NOT EXISTS `nyc-mobility`.nyc_bronze.taxi_zones
 (
-    LocationID INT,
-    Borough STRING,
-    Zone STRING,
+    location_id INT,
+    borough STRING,
+    zone STRING,
     service_zone STRING,
     ingestion_time TIMESTAMP,
     source_file STRING
