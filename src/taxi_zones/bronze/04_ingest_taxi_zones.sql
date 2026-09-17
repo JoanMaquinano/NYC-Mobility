cd6825d44c10f69e0d03dd8bccfcd4c4e073b80e
via.cabuyao@ftwfoundation.org <via.cabuyao@ftwfoundation.org>
2026-09-17 06:30:59 +0000

    fixed/ingest_taxi_zone
    fixed/ingest_taxi_zone

--- src/taxi_zones/bronze/04_ingest_taxi_zones.sql
+++ src/taxi_zones/bronze/04_ingest_taxi_zones.sql
@@ -1,8 +1,11 @@
-INSERT INTO `nyc-mobility`.nyc_bronze.taxi_zones
+CREATE TABLE IF NOT EXISTS `nyc-mobility`.nyc_bronze.taxi_zones 
+USING DELTA
+TBLPROPERTIES ('delta.columnMapping.mode' = 'name')
+AS
 SELECT
-    LocationID,
-    Borough,
-    Zone,
+    LocationID AS location_id,
+    Borough AS borough,
+    Zone AS zone,
     service_zone,
     current_timestamp() AS ingestion_time,
     _metadata.file_path AS source_file
