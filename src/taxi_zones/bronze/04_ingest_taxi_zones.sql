37f6e94322009087cb9bec57c54e8bc27dde75f5
via.cabuyao@ftwfoundation.org <via.cabuyao@ftwfoundation.org>
2026-09-17 16:07:18 +0000

    fixed/bronze_taxi
    fixed/bronze_taxi

--- src/taxi_zones/bronze/04_ingest_taxi_zones.sql
+++ src/taxi_zones/bronze/04_ingest_taxi_zones.sql
@@ -1,4 +1,4 @@
-INSERT INTO `nyc-mobility`.nyc_bronze.taxi_zones
+CREATE OR REPLACE TABLE `nyc-mobility`.nyc_bronze.taxi_zones
 SELECT
     LocationID AS location_id,
     Borough AS borough,
@@ -10,4 +10,8 @@ FROM read_files(
     '/Volumes/workspace/default/ftw-b12-de/groups/week-08/group-d/taxi-zone-lookup/',
     format => 'csv',
     header => 'true'
+)
+USING DELTA
+TBLPROPERTIES (
+    'delta.columnMapping.mode' = 'name'
 );
